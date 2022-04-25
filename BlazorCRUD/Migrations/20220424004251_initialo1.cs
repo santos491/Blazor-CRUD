c1c583f8def732ee20d6e32e6cbcbacfee8d7ee2
santos <antonioars857@gmail.com>
2022-04-25 18:19:17 -0500

    first commit

---
+++ BlazorCRUD/Migrations/20220424004251_initialo1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCRUD.Migrations
+{
+    public partial class initialo1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
